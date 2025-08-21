b9f0afe10827f37dc5dc31b17720664076a17a7f
Sam Black <samblack@gmail.com>
2025-08-21 11:02:57 -0500

    Day 1 notes

---
+++ src/Unit1/part0a/FirstFile.java
@@ -0,0 +1,12 @@
+package Unit1.part0a;
+
+public class FirstFile {
+    public static void main(String[] args) {
+        System.out.println("Hello Patriots!!");
+        System.out.print("Howdy");
+//        System.out.print(" ");
+        System.out.println("Patriots!");
+
+        System.out.println("Hello\n Pat\t\"riots!\\");
+    }
+}
