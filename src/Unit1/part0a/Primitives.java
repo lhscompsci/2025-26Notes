7acd252229e01b858a62405b482473709d2361c7
Sam Black <samblack@gmail.com>
2025-08-28 11:11:50 -0500

    data type notes

---
+++ src/Unit1/part0a/Primitives.java
@@ -0,0 +1,53 @@
+package Unit1.part0a;
+
+/**
+ * this is a javadoc comment
+ */
+public class Primitives {
+    /**
+     * this is the main method
+     * @param args
+     */
+    public static void main(String[] args) {
+        // this is a comment
+        /*
+
+        multi line comments
+         */
+        //8 primitive data types
+        // 6-ish are numeric
+        // 1-ish is symbolic
+        // boolean in nature
+
+        //numeric world
+        //4 integer types --
+        // byte - 8-bit             -128 to 127
+        // short - 16-bit           -32768 to 32767
+        // int - 32-bit  MOST USED  -2147483648 to 2147483647
+        // long - 64-bit            -9quint to 9quint
+
+        // 2 real types --
+        // float - 32-bit       7-8 digits of accuracy
+        // double - 64-bit      15-16 digits of accuracy
+
+        // character data
+        // char - 16-bit            0 to 65535
+        // A => 65
+        // a => 97
+        // 0 => 48
+        // store the data in ' '
+
+        // boolean data
+        // true
+        // false
+
+        int count = 0;
+        count = 2;
+        System.out.println("The value of count is " + count);
+
+        // string
+        String name = "George";
+
+
+    }
+}
